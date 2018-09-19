6ec360884253ab3b0e1ef0a3cdcd5c26b3988361
Greg Albiston <4662258+galbiston@users.noreply.github.com>
2018-09-19 19:14:12 +0100

    MainTest: comments.

---
+++ src/test/java/geosparql_fuseki/MainTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geosparql_fuseki;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ *
+ * @author Gerg
+ */
+public class MainTest {
+
+    public MainTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Main.
+     */
+    @Test
+    @Ignore
+    public void testMain() {
+        System.out.println("main");
+        String[] args = {"-rf", "geosparql_conformance.rdf", "-re", "xml"};
+        Main.main(args);
+        //Need to disable server from starting otherwise thread blocks.
+    }
+
+}
