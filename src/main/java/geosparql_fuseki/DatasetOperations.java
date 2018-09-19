296b9fb5b43b871f0369d58c49aea1a3c3fbacb0
Greg Albiston <4662258+galbiston@users.noreply.github.com>
2018-09-19 12:08:05 +0100

    DatasetOperations: methods for setting up dataset.

---
+++ src/main/java/geosparql_fuseki/DatasetOperations.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geosparql_fuseki;
+
+import geosparql_fuseki.cli.ArgsConfig;
+import geosparql_jena.implementation.data_conversion.GeoSPARQLPredicates;
+import java.io.File;
+import java.lang.invoke.MethodHandles;
+import java.util.Iterator;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.tdb.TDBFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rdf_tables.cli.SeparatorValidator;
+import rdf_tables.file.FileReader;
+
+/**
+ *
+ *
+ */
+public class DatasetOperations {
+
+    private static final File GEOSPARQL_SCHEMA_FILE = new File(DatasetOperations.class.getClassLoader().getResource("geosparql_vocab_all_v1_0_1_updated.rdf").getFile());
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    public static Dataset prepare(ArgsConfig argsConfig) {
+
+        Dataset dataset;
+        File tdbFolder = argsConfig.getTdbFile();
+        if (tdbFolder != null) {
+            dataset = TDBFactory.createDataset(tdbFolder.getAbsolutePath());
+        } else {
+            dataset = DatasetFactory.create();
+        }
+
+        return dataset;
+    }
+
+    public static void loadData(ArgsConfig argsConfig, Dataset dataset) {
+
+        if (argsConfig.getRdfFile() != null) {
+
+            try {
+                //Default Model
+                dataset.begin(ReadWrite.WRITE);
+                Model defaultModel = dataset.getDefaultModel();
+                Model model = RDFDataMgr.loadModel(argsConfig.getRdfFile().getAbsolutePath(), argsConfig.getRdfFormat().getLang());
+                defaultModel.add(model);
+                dataset.commit();
+
+            } catch (Exception ex) {
+                LOGGER.error("Write Error: {}", ex.getMessage());
+            } finally {
+                dataset.end();
+            }
+
+        }
+
+        if (argsConfig.getTabularFile() != null) {
+            try {
+                //Default Model
+                dataset.begin(ReadWrite.WRITE);
+                Model defaultModel = dataset.getDefaultModel();
+                Model model = FileReader.convertCSVFile(argsConfig.getTabularFile(), SeparatorValidator.getSeparatorCharacter(argsConfig.getTabularSeparator()));
+                defaultModel.add(model);
+                dataset.commit();
+
+            } catch (Exception ex) {
+                LOGGER.error("Write Error: {}", ex.getMessage());
+            } finally {
+                dataset.end();
+            }
+        }
+
+    }
+
+    public static void applyDefaultGeometry(ArgsConfig argsConfig, Dataset dataset) {
+        if (argsConfig.isApplyDefaultGeometry()) {
+
+            try {
+                //Default Model
+                dataset.begin(ReadWrite.WRITE);
+
+                //Default Model
+                dataset.begin(ReadWrite.WRITE);
+                Model defaultModel = dataset.getDefaultModel();
+                GeoSPARQLPredicates.applyDefaultGeometry(defaultModel);
+
+                //Named Models
+                Iterator<String> graphNames = dataset.listNames();
+                while (graphNames.hasNext()) {
+                    String graphName = graphNames.next();
+                    Model namedModel = dataset.getNamedModel(graphName);
+                    GeoSPARQLPredicates.applyDefaultGeometry(namedModel);
+                }
+
+                dataset.commit();
+
+            } catch (Exception ex) {
+                LOGGER.error("Write Error: {}", ex.getMessage());
+            } finally {
+                dataset.end();
+            }
+        }
+    }
+
+    public static void applyInferencing(ArgsConfig argsConfig, Dataset dataset) {
+
+        if (argsConfig.isInference()) {
+            Model geosparqlSchema = RDFDataMgr.loadModel(GEOSPARQL_SCHEMA_FILE.getAbsolutePath());
+
+            try {
+                //Default Model
+                dataset.begin(ReadWrite.WRITE);
+                Model defaultModel = dataset.getDefaultModel();
+                applySchema(geosparqlSchema, defaultModel);
+
+                //Named Models
+                Iterator<String> graphNames = dataset.listNames();
+                while (graphNames.hasNext()) {
+                    String graphName = graphNames.next();
+                    Model namedModel = dataset.getNamedModel(graphName);
+                    applySchema(geosparqlSchema, namedModel);
+                }
+
+                dataset.commit();
+
+            } catch (Exception ex) {
+                LOGGER.error("Inferencing Error: {}", ex.getMessage());
+            } finally {
+                dataset.end();
+            }
+        }
+    }
+
+    public static void applySchema(Model model, Model geosparqlSchema) {
+        if (!model.isEmpty()) {
+            InfModel infModel = ModelFactory.createRDFSModel(geosparqlSchema, model);
+            model.add(infModel.getDeductionsModel());
+        }
+    }
+
+}
